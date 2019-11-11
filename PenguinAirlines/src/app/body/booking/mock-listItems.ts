98ac3babb9188e273cd5826f11cb4b9e2f38d5dc
Post <ivar.post@klm.com>
2019-11-11 16:51:34 +0100

    added more flights

--- PenguinAirlines/src/app/body/booking/mock-listItems.ts
+++ PenguinAirlines/src/app/body/booking/mock-listItems.ts
@@ -7,4 +7,22 @@ export let flightsBooked: flightBooked[] = [
         userId: 0,
         class: 'Economy',
     },
+    { 
+        id: 1, 
+        flightId: 0,
+        userId: 1,
+        class: 'Business',
+    },
+    { 
+        id: 2, 
+        flightId: 1,
+        userId: 0,
+        class: 'Business',
+    },
+    { 
+        id: 3, 
+        flightId: 0,
+        userId: 0,
+        class: 'Economy',
+    },
 ];
